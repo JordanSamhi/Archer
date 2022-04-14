0616eb6805c1e773c40187ed1aefb85c89c20d25
Jordan Samhi <j.samhi@me.com>
2022-04-13 20:50:59 -0700

    Add: sootconfig for completing soot options

--- src/main/java/lu/uni/archer/config/SootConfig.java
+++ src/main/java/lu/uni/archer/config/SootConfig.java
@@ -1,14 +1,34 @@
 package lu.uni.archer.config;
 
-import lu.uni.archer.utils.CommandLineOptions;
-import soot.Scene;
 import soot.jimple.infoflow.InfoflowConfiguration;
 import soot.jimple.infoflow.config.IInfoflowConfig;
 import soot.options.Options;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+/*-
+ * #%L
+ * Archer
+ *
+ * %%
+ * Copyright (C) 2022 Jordan Samhi
+ * University of Luxembourg - Interdisciplinary Centre for
+ * Security Reliability and Trust (SnT) - TruX - All rights reserved
+ *
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 
 public class SootConfig implements IInfoflowConfig {
 
