dc19ba534832215d4d34d9b5431ec123c8b6c08f
Jordan Samhi <j.samhi@me.com>
2022-04-19 15:48:37 -0700

    Update: source sink manager

--- src/main/java/lu/uni/archer/files/SourcesSinksManager.java
+++ src/main/java/lu/uni/archer/files/SourcesSinksManager.java
@@ -10,63 +10,63 @@ import java.util.Set;
 
 public class SourcesSinksManager extends FileLoader {
 
-	private static SourcesSinksManager instance;
-	private final Set<AndroidMethod> sources;
-	private final Set<AndroidMethod> sinks;
+    private static SourcesSinksManager instance;
+    private final Set<AndroidMethod> sources;
+    private final Set<AndroidMethod> sinks;
 
-	private SourcesSinksManager() {
-		super();
-		this.sources = new HashSet<>();
-		this.sinks = new HashSet<>();
-		this.loadSourcesSinks();
-	}
+    private SourcesSinksManager() {
+        super();
+        this.sources = new HashSet<>();
+        this.sinks = new HashSet<>();
+        this.loadSourcesSinks();
+    }
 
-	public static SourcesSinksManager v() {
-		if(instance == null) {
-			instance = new SourcesSinksManager();
-		}
-		return instance;
-	}
+    public static SourcesSinksManager v() {
+        if (instance == null) {
+            instance = new SourcesSinksManager();
+        }
+        return instance;
+    }
 
-	private void loadSourcesSinks() {
-		String[] split;
-		String type, signature;
-		for(String method: this.items) {
-			split = method.split("\\|");
-			if(split.length == 2) {
-				type = split[0];
-				signature = split[1];
-				AndroidMethod am = new AndroidMethod(Utils.getMethodNameFromSignature(signature),
-						Utils.getParametersNamesFromSignature(signature),
-						Utils.getReturnNameFromSignature(signature),
-						Utils.getClassNameFromSignature(signature));
-				if(type.equals(Constants.SOURCE)) {
-					this.sources.add(am);
-				}else if(type.equals(Constants.SINK)) {
-					this.sinks.add(am);
-				}
-			}
-		}
-	}
+    private void loadSourcesSinks() {
+        String[] split;
+        String type, signature;
+        for (String method : this.items) {
+            split = method.split("\\|");
+            if (split.length == 2) {
+                type = split[0];
+                signature = split[1];
+                AndroidMethod am = new AndroidMethod(Utils.getMethodNameFromSignature(signature),
+                        Utils.getParametersNamesFromSignature(signature),
+                        Utils.getReturnNameFromSignature(signature),
+                        Utils.getClassNameFromSignature(signature));
+                if (type.equals(Constants.SOURCE)) {
+                    this.sources.add(am);
+                } else if (type.equals(Constants.SINK)) {
+                    this.sinks.add(am);
+                }
+            }
+        }
+    }
 
-	public Set<AndroidMethod> getSources() {
-		return this.sources;
-	}
+    public Set<AndroidMethod> getSources() {
+        return this.sources;
+    }
 
-	public Set<AndroidMethod> getSinks() {
-		return this.sinks;
-	}
+    public Set<AndroidMethod> getSinks() {
+        return this.sinks;
+    }
 
-	public void addSink(SootMethod sm) {
-		this.sinks.add(new AndroidMethod(sm));
-	}
-	
-	public void addSource(SootMethod sm) {
-		this.sources.add(new AndroidMethod(sm));
-	}
+    public void addSink(SootMethod sm) {
+        this.sinks.add(new AndroidMethod(sm));
+    }
 
-	@Override
-	protected String getFile() {
-		return Constants.SOURCES_SINKS_FILE;
-	}
+    public void addSource(SootMethod sm) {
+        this.sources.add(new AndroidMethod(sm));
+    }
+
+    @Override
+    protected String getFile() {
+        return Constants.SOURCES_SINKS_FILE;
+    }
 }
