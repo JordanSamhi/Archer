0cc2431c41a9697810824dd13cb24b59fbb5204b
Jordan Samhi <j.samhi@me.com>
2022-04-21 14:58:37 -0700

    Add: build script

---
+++ build.sh
@@ -0,0 +1,2 @@
+mvn clean install:install-file -Dfile=libs/soot-infoflow-android-classes.jar -DgroupId=lu.uni.trux -DartifactId=soot-infoflow-android-modified -Dversion=2.7.1 -Dpackaging=jar
+mvn clean install
