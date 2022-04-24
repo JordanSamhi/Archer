998d03c58e23891aeda939ddd79a5db0acda5ca3
Jordan Samhi <j.samhi@me.com>
2022-04-24 11:30:43 -0700

    Update: build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+export MAVEN_OPTS=-Xss32m
 mvn clean install:install-file -Dfile=libs/soot-infoflow-android-classes.jar -DgroupId=lu.uni.trux -DartifactId=soot-infoflow-android-modified -Dversion=2.7.1 -Dpackaging=jar
 mvn clean install
