4972dc4fb27faa6e6363c04549ab2fe1ae227ff6
Jordan Samhi <j.samhi@me.com>
2024-01-14 14:31:54 +0100

    Update: script

--- artifacts/rq2_improve_static_analysis/rq2b_wild/run.sh
+++ artifacts/rq2_improve_static_analysis/rq2b_wild/run.sh
@@ -1,7 +1,22 @@
-apk=$1
-platforms=$2
-redisServer=$3
-redisPort=$4
-redisPwd=$5
-java -jar archer.jar -a $apk -p $platforms -s $redisServer -n $redisPort -w $redisPwd
-rm $apk
+#!/bin/bash
+
+# Initialize variables from script arguments
+apk="$1"
+platforms="$2"
+redisServer="$3"
+redisPort="$4"
+redisPwd="$5"
+
+# Initialize an empty string to hold optional parameters
+optional_params=""
+
+# Conditionally add optional parameters
+[ -n "$redisServer" ] && optional_params+=" -s $redisServer"
+[ -n "$redisPort" ] && optional_params+=" -n $redisPort"
+[ -n "$redisPwd" ] && optional_params+=" -w $redisPwd"
+
+# Run the Java command with conditional parameters
+java -jar archer.jar -a "$apk" -p "$platforms" $optional_params
+
+# Remove apk
+rm "$apk"
