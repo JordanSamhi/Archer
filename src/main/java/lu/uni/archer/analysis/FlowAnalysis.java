6fd660de9be55cbd34d5334ab1f56190ad996578
Jordan Samhi <j.samhi@me.com>
2024-01-14 14:30:13 +0100

    Add: check leak through CI call

--- src/main/java/lu/uni/archer/analysis/FlowAnalysis.java
+++ src/main/java/lu/uni/archer/analysis/FlowAnalysis.java
@@ -1,8 +1,11 @@
 package lu.uni.archer.analysis;
 
 import heros.solver.Pair;
+import lu.uni.archer.files.ExecuteesManager;
 import lu.uni.archer.files.SourcesSinksManager;
 import lu.uni.archer.utils.Writer;
+import soot.Scene;
+import soot.SootMethod;
 import soot.jimple.Stmt;
 import soot.jimple.infoflow.android.InfoflowAndroidConfiguration;
 import soot.jimple.infoflow.android.SetupApplication;
@@ -12,24 +15,29 @@ import soot.jimple.infoflow.results.ResultSourceInfo;
 import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
 import soot.util.MultiMap;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class FlowAnalysis {
     private final SetupApplication sa;
     private InfoflowResults ir;
+
+    private InfoflowCFG icfg;
 
     public FlowAnalysis(SetupApplication sa) {
         this.sa = sa;
         sa.getConfig().setSootIntegrationMode(InfoflowAndroidConfiguration.SootIntegrationMode.UseExistingCallgraph);
         sa.getConfig().getPathConfiguration().setPathReconstructionMode(InfoflowAndroidConfiguration.PathReconstructionMode.Precise);
         sa.getConfig().setCodeEliminationMode(InfoflowAndroidConfiguration.CodeEliminationMode.NoCodeElimination);
+        this.icfg = new InfoflowCFG();
     }
 
     public void runTaintAnalysis() {
         try {
             this.ir = sa.runInfoflow(SourcesSinksManager.v().getSources(), SourcesSinksManager.v().getSinks());
         } catch (Exception ignored) {
-        	ignored.printStackTrace();
+            ignored.printStackTrace();
         }
     }
 
@@ -56,10 +64,23 @@ public class FlowAnalysis {
                     for (Stmt s : path) {
                         System.out.println("      -" + s + " => in method: " + icfg.getMethodOf(s));
                     }
+                    if (pathContainCallToImplicitMechanism(Arrays.asList(path))) {
+                        System.out.println("Leak through implicit call");
+                    }
                 }
             }
         } else {
             Writer.v().pwarning("No leak found");
         }
+    }
+
+    private boolean pathContainCallToImplicitMechanism(List<Stmt> path) {
+        for (Stmt s : path) {
+            SootMethod sm = icfg.getMethodOf(s);
+            if (ExecuteesManager.v().isExecutee(sm)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
